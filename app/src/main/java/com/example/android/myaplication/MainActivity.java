5bbfc64b354a2a4f480e9689acc0afd653305ca8
Miguel Angel González Pacheco <sedensomega@hotmail.com>
2018-09-12 21:52:23 -0500

    Development Branch, first activity

--- app/src/main/java/com/example/android/myaplication/MainActivity.java
+++ app/src/main/java/com/example/android/myaplication/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.myaplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.android.myaplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void aceptAccion(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        EditText txtFirstName = findViewById(R.id.txtFirstName);
+        EditText txtLastName = findViewById(R.id.txtLastName);
+        String message = R.string.str_welcome + " " + txtFirstName.getText().toString() + " " + txtLastName.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
